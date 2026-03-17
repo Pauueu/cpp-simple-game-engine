63c336d30e97a316cbb3875ff16e65c237b8d786
Paweł Siwiec <paweu622@gmail.com>
2026-03-18 00:05:30 +0100

    Stworzenie pliku ResourceManager.hpp i utworzenie szablonu klasy ResourceManager

---
+++ Engine/include/ResourceManager.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <map>
+#include <string>
+
+class ResourceManager {
+public:
+    ResourceManager() = default;
+
+    // Funkcje do zapisu zasobów
+    sf::Texture& getTexture(const std::string& path);
+    // sf::Font& getFont(const std::string& path);
+    // sf::SoundBuffer& getSoundBuffer(const std::string& path);
+
+    // Funkcja do czyszczenia zasobów
+    void clear();
+
+private:
+    // Mapy do przechowywania zasobów
+    std::map<std::string, sf::Texture> textures;
+    std::map<std::string, sf::Font> fonts;
+    std::map<std::string, sf::SoundBuffer> soundBuffers;
+};
