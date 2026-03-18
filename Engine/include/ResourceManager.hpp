01cd927bf0dc981a5d347d70f025cc33f87265ea
Paweł Siwiec <paweu622@gmail.com>
2026-03-18 10:41:12 +0100

    Narazie pominięcie obsługi dźwięku w ResourceManager

--- Engine/include/ResourceManager.hpp
+++ Engine/include/ResourceManager.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
+// #include <SFML/Audio.hpp>
 #include <map>
 #include <string>
 
@@ -10,7 +10,7 @@ public:
 
     // Funkcje do zapisu zasobów
     sf::Texture& getTexture(const std::string& path);
-    // sf::Font& getFont(const std::string& path);
+    sf::Font& getFont(const std::string& path);
     // sf::SoundBuffer& getSoundBuffer(const std::string& path);
 
     // Funkcja do czyszczenia zasobów
@@ -20,5 +20,5 @@ private:
     // Mapy do przechowywania zasobów
     std::map<std::string, sf::Texture> textures;
     std::map<std::string, sf::Font> fonts;
-    std::map<std::string, sf::SoundBuffer> soundBuffers;
+    // std::map<std::string, sf::SoundBuffer> soundBuffers;
 };
