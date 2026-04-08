67a36b25c6fd5d5214735a1c02c14ef9bfefedbd
Paweł Siwiec <paweu622@gmail.com>
2026-04-08 06:47:31 +0200

    Testowe wykorzystanie InputManager

--- Game/include/player.hpp
+++ Game/include/player.hpp
@@ -3,7 +3,13 @@
 
 // Klasa Player dziedziczy po klasie GameObject
 class Player : public GameObject {
+private:
+    float speed;
+
+protected:
+    void onUpdate(float dt);
+
 public:
     // Konstruktor
-    Player(const std::string& path, float startX, float startY);
+    Player(const std::string& path, float startX, float startY);  
 };
--- Game/src/player.cpp
+++ Game/src/player.cpp
@@ -1,6 +1,29 @@
 #include "Player.hpp"
+#include "InputManager.hpp"
+#include <iostream>
 
 Player::Player(const std::string& path, float startX, float startY): GameObject(path, startX, startY) 
 {
+    speed = 300.0f;
+}
+
+void Player::onUpdate(float deltaTime) {
     
+    // Sterowanie graczem
+    if (InputManager::GetKey(KeyCode::W)) {
+        position.y -= speed * deltaTime; // W górę
+    }
+    if (InputManager::GetKey(KeyCode::S)) {
+        position.y += speed * deltaTime; // W dół
+    }
+    if (InputManager::GetKey(KeyCode::A)) {
+        position.x -= speed * deltaTime; // W lewo
+    }
+    if (InputManager::GetKey(KeyCode::D)) {
+        position.x += speed * deltaTime; // W prawo
+    }
+    if (InputManager::GetKeyDown(KeyCode::MouseLeft)) {
+        std::cout<<"Nacisnieto przycisk \n" << static_cast<int>(KeyCode::Count);
+
+    }
 }
