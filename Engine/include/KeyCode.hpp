e4e20b8db918e82b5ee0b9e800db271c8266fbd2
Paweł Siwiec <paweu622@gmail.com>
2026-04-14 20:12:12 +0200

    Rozszerzenie przycisków możłiwych do wykorzystania w Input Manager

--- Engine/include/KeyCode.hpp
+++ Engine/include/KeyCode.hpp
@@ -1,18 +1,45 @@
 // Klasa zawierająca wszystkie możliwe przycisku do użycia w Input Manager
 
 enum class KeyCode {
-    A, B, C, D,
-    E, F, G, H,
-    I, J, K, L,
-    M, N, O, P,
-    Q, R, S, T,
-    U, V, W, X,
+    A, B, C,
+    D, E, F,
+    G, H, I,
+    J, K, L,
+    M, N, O,
+    P, Q, R,
+    S, T, U,
+    V, W, X,
     Y, Z,
-    
-    UpArrow, DownArrow, LeftArrow, RightArrow,
-    Space, Escape, LeftAlt, RightAlt,
-    LeftCtrl, RightCtrl, Enter, LeftShift, 
-    RightShift, Tab, CapsLock, Backspace,
+
+    Num0, Num1, Num2,
+    Num3, Num4, Num5,
+    Num6, Num7, Num8,
+    Num9,
+
+    Add, Apostrophe, Backslash,
+    Backspace, Comma, Delete,
+    Divide, Down, End,
+    Enter, Equal, Escape,
+    Grave, Home, Hyphen, 
+    Insert, LeftAlt, LeftBeacket,
+    LeftCtrl, Left, LeftShift,
+    LeftSystem, Menu, Multiply,
+    PageDown, PageUp, Period,
+    RightAlt, RightBracket, RightCtrl,
+    Right, RightShift, RightSystem,
+    Semicolon, Slash, Space,
+    Subtract, Tab, Up,
+
+    Numpad0, Numpad1, Numpad2,
+    Numpad3, Numpad4, Numpad5,
+    Numpad6, Numpad7,Numpad8,
+    Numpad9,
+
+    F1, F2, F3,
+    F4, F5, F6,
+    F7, F8, F9,
+    F10, F11, F12,
+    F13, F14, F15,
 
     MouseLeft, MouseRight, MouseMiddle,
 
